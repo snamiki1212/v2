57ccce0ac37228749d7c57c5e25c85327bac9281
snamiki1212 <snamiki1212@gmail.com>
2021-02-10 17:09:31 -0800

    Update index

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script lang="typescript">
   import HomePage from "./components/pages/HomePage/index.svelte";
+  const GITHUB_URL = "https://github.com/snamiki1212/v2";
+  const msg = `🦊🐳🐱🦊🐳🐱🦊🐳🐱\n🦊🐳🐱🦊🐳🐱🦊🐳🐱\n🦊🐳🐱🦊🐳🐱🦊🐳🐱\n\nHi :)\n You want to know more\n Check code!\n => ${GITHUB_URL}\n\n`;
+  console.log(msg);
 </script>
 
 <main>
