9cb0cd10ed70164b887459bbc1a629c6907bbd43
snamiki1212 <snamiki1212@gmail.com>
2021-02-10 17:00:29 -0800

    Add section-experience in svelte

--- src/components/pages/HomePage/index.svelte
+++ src/components/pages/HomePage/index.svelte
@@ -1,9 +1,11 @@
 <script lang="typescript">
   import SectionHero from "../../sections/section-hero/index.svelte";
   import SectionAboutMe from "../../sections/section-aboutme/index.svelte";
+  import SectionExperiences from "../../sections/section-experiences/index.svelte";
 </script>
 
 <div>
   <SectionHero />
   <SectionAboutMe />
+  <SectionExperiences />
 </div>
---
+++ src/components/sections/section-experiences/index.svelte
@@ -0,0 +1,100 @@
+<div class="section-experiences">
+  <div class="section-experiences__title">
+    <div class="subtitle">
+      <span class="subtitle__num">02. </span>
+      <span class="subtitle__text">Experiences</span>
+    </div>
+  </div>
+  <div class="section-experiences__content">
+    <div class="experience-list">
+      <div class="experience-list__item">
+        <div>Fullstack Developer as Freelance</div>
+        <div>🕓2019-Jan ~ Present</div>
+        <div>👔Part-time</div>
+        <ul class="experiences-list">
+          <li class="experiences-list__item">
+            Collaborated with more than each 5 companies
+          </li>
+          <li class="experiences-list__item">
+            Developed a variety of apps using React, React Native, Next.js,
+            TypeScript, Firebase and Ruby on Rails
+          </li>
+        </ul>
+      </div>
+      <div class="experience-list__item">
+        <div>
+          Backend Developer<a href="https://www.arsaga.jp/">@Arsaga Partners</a>
+        </div>
+        <div>🕓2017-Jun ~ 2018-Dec</div>
+        <div>👔Full-time</div>
+        <ul class="experiences-list">
+          <li class="experiences-list__item">
+            Developed web services using PHP/Laravel, jQuery
+          </li>
+          <li class="experiences-list__item">
+            Developed smartphone game using Elixir/Phoenix
+          </li>
+          <li class="experiences-list__item">Architected APIs</li>
+          <li class="experiences-list__item">
+            Collaborated with members as a leader
+          </li>
+        </ul>
+      </div>
+      <div class="experience-list__item">
+        <div>
+          Project Leader<a href="https://www.nttdata-getronics.co.jp/en/"
+            >@NTT Data Getronics</a
+          >
+        </div>
+        <div>🕓2013-Apr ~ 2017-May</div>
+        <div>👔Full-time</div>
+        <ul class="experiences-list">
+          <li class="experiences-list__item">Managed small team</li>
+          <li class="experiences-list__item">Coordinated with client</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="section-experiences__bottom">
+    Jump to <a href="https://www.linkedin.com/in/snamiki1212/">linkedin</a> if you
+    want to know more!
+  </div>
+</div>
+
+<style lang="scss">
+  .section-experiences {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    &__content {
+      padding: 2rem;
+      border: 1px solid var(--cyan-transparent-05);
+    }
+    &__bottom {
+      text-align: center;
+    }
+  }
+
+  .experience-list {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 2rem;
+
+    &__item {
+      padding: 2rem;
+      border: 1px solid var(--cyan-transparent-05);
+      background: var(--cyan-transparent-01);
+    }
+  }
+  .experiences-list {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 0.5rem;
+    &__item {
+      color: var(--yellow-transparent-08);
+      font-size: 0.9rem;
+    }
+  }
+</style>
