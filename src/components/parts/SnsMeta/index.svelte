e735c574be8c2c8a52b9e76858824b6bc12972e7
snamiki1212 <snamiki1212@gmail.com>
2021-02-13 14:43:24 -0800

    Update head tag

--- src/components/parts/SnsMeta/index.svelte
+++ src/components/parts/SnsMeta/index.svelte
@@ -6,25 +6,27 @@
   const OGP_DESCRIPTION = "Portfolio | Namiki Shun a.k.a Nash";
 </script>
 
-<!-- OGP -->
-<meta name="description" content="Portfolio v2 | Namiki Shun a.k.a Nash" />
+<svelte:head>
+  <!-- OGP -->
+  <meta name="description" content={OGP_DESCRIPTION} />
 
-<!-- ogp -->
-<meta property="og:type" content="website" />
-<meta property="og:title" content={OGP_PAGE_TITLE} />
-<meta property="og:description" content={OGP_DESCRIPTION} />
-<meta property="og:image" content={OGP_IMAGE_PATH} />
-<meta property="og:image:alt" content={OGP_IMAGE_PATH} />
-<meta property="og:url" content={THIS_GITHUB_URL} />
-<meta property="og:site_name" content={OGP_SITE_NAME} />
+  <!-- ogp -->
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content={OGP_PAGE_TITLE} />
+  <meta property="og:description" content={OGP_DESCRIPTION} />
+  <meta property="og:image" content={OGP_IMAGE_PATH} />
+  <meta property="og:image:alt" content={OGP_IMAGE_PATH} />
+  <meta property="og:url" content={THIS_GITHUB_URL} />
+  <meta property="og:site_name" content={OGP_SITE_NAME} />
 
-<!-- facebook -->
-<!-- <meta property="article:author" content="https://www.facebook.com/xxxnamexxx" /> -->
-<!-- <meta property="fb:admins" content="123456789" /> -->
+  <!-- facebook -->
+  <!-- <meta property="article:author" content="https://www.facebook.com/xxxnamexxx" /> -->
+  <!-- <meta property="fb:admins" content="123456789" /> -->
 
-<!-- twitter card -->
-<meta name="twitter:card" content="summary" />
-<meta name="twitter:title" content={OGP_PAGE_TITLE} />
-<meta name="twitter:description" content={OGP_DESCRIPTION} />
-<meta name="twitter:image:src" content={OGP_IMAGE_PATH} />
-<meta name="twitter:site" content={MY_TWITTER_ACCOUNT} />
+  <!-- twitter card -->
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:title" content={OGP_PAGE_TITLE} />
+  <meta name="twitter:description" content={OGP_DESCRIPTION} />
+  <meta name="twitter:image:src" content={OGP_IMAGE_PATH} />
+  <meta name="twitter:site" content={MY_TWITTER_ACCOUNT} />
+</svelte:head>
