feacc9168d23ed02540a9c37e51b0b6edc84a1fd
snamiki1212 <snamiki1212@gmail.com>
2021-02-13 17:08:03 -0800

    Fix seo tag from CSR to bare html

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,12 @@
 <script lang="typescript">
   import HomePage from "components/pages/HomePage/index.svelte";
   import EasterEgg from "components/parts/EasterEgg/index.svelte";
-  import SnsMeta from "components/parts/SnsMeta/index.svelte";
 </script>
 
 <!-- Meta -->
 <svelte:head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <SnsMeta />
 </svelte:head>
 
 <!-- Page -->
